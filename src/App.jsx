708ae2581b9e20fedd383ff3eff3a1de9913f2e1
kevinesquea17 <kevinesq1996@gmail.com>
2023-02-22 16:58:10 -0500

    responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <main className='font-Rajdhani bg-gray-100'>
       <section className='py-10 max-w-[70rem] mx-auto'>
-          <h2 className='text-4xl text-center font-semibold text-gray-800 uppercase tracking-widest'>Recents Projects</h2>
+          <h2 className='sm:text-4xl text-2xl text-center font-semibold text-gray-800 uppercase'>Recents Projects</h2>
           <div className='w-full mt-10'>
             <ListProject />
           </div>
