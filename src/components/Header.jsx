9a4a2b41aa57ed70c98f7bbef2dba90243261e9c
kevinesquea17 <kevinesq1996@gmail.com>
2023-03-31 11:39:40 -0500

    change height of logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className='flex items-center h-[90px] absolute top-0 left-0 w-full z-10'>
         <div className='flex items-center justify-between w-[80%] max-w-[80%] mx-auto'>
-            <img src={Logo} alt="" className='w-32 object-cover' />
+            <img src={Logo} alt="" className='w-32 h-auto object-cover' />
             <nav className='sm:flex items-center space-x-6 8 text-lg uppercase font-normal text-gray-800 hidden'>
                 <a href="#" className='hover:text-blue-600 transition duration-300 ease-out'>Home</a>
                 <a href="#" className='hover:text-blue-600 transition duration-300 ease-out'>About</a>
