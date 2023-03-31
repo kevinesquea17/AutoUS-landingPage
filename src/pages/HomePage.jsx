315bf2d5ea4f7d00fb1b0ebc08a350d024284dc1
kevinesquea17 <kevinesq1996@gmail.com>
2023-03-31 13:45:37 -0500

    añadiendo helmet para añadir meta

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from "react-helmet";
 import RecentProjects from '../components/RecentProjects'
 import FrontPage from '../components/FrontPage'
 import SectionServices from '../components/SectionServices'
@@ -9,6 +10,9 @@ import { SliderProvider } from '../context/SliderContext'
 const HomePage = () => {
   return (
     <SliderProvider>
+      <Helmet>
+        <meta name="description" content="page home of landing of services drone" />
+      </Helmet>
       <main className='font-Rajdhani'>
         <FrontPage />
         <SectionServices />
